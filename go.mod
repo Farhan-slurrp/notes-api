8c780ba4e850f694dd902961e28f4c9a5b70290d
Farhan-slurrp <farhanlmntrix@gmail.com>
2021-07-11 10:53:06 +0800

    set postgres db

--- go.mod
+++ go.mod
@@ -4,8 +4,16 @@ module github.com/Farhan-slurrp/go-notes
 go 1.16
 
 require (
+	github.com/alexbrainman/sspi v0.0.0-20180613141037-e580b900e9f5 // indirect
 	github.com/gofiber/fiber/v2 v2.14.0
-	github.com/lib/pq v1.6.0 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.2.0 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+	gopkg.in/jcmturner/aescts.v1 v1.0.1 // indirect
+	gopkg.in/jcmturner/dnsutils.v1 v1.0.1 // indirect
+	gopkg.in/jcmturner/goidentity.v3 v3.0.0 // indirect
+	gopkg.in/jcmturner/gokrb5.v7 v7.5.0 // indirect
+	gopkg.in/jcmturner/rpc.v1 v1.1.0 // indirect
+	gorm.io/driver/postgres v1.1.0
 	gorm.io/driver/sqlite v1.1.4 // indirect
 	gorm.io/gorm v1.21.11 // indirect
 )
--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+
 	"github.com/Farhan-slurrp/go-notes/database"
 	"github.com/Farhan-slurrp/go-notes/notes"
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/sqlite"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func initDB() {
 	var err error
-	database.DBConn, err = gorm.Open(sqlite.Open("notes.db"), &gorm.Config{}) 
+	database.DBConn, err = gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{}) 
 
 	if err != nil {
 		panic("Database error")
@@ -44,9 +46,9 @@ func main() {
 	port, ok := os.LookupEnv("PORT")
 
     	if ok == false {
-        	port = "3000"
+        	port = "4000"
     	}
 
 	app.Listen(":"+port)
-	fmt.Println("Listening on port 4000🚀")
+	fmt.Println("Listening on port", port, "🚀")
 }
